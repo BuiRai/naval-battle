b5a6a267e127492466ba3fa77553a1c7acd808f2
buirai <buirai_naetle@hotmail.com>
2016-09-01 17:37:44 -0500

    dashboard logic finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var navalGame = (function(){
 	console.log("Init game...");
+
 	var game = {};
+
 	game.dashboard = [
 		[{id: 0, value: false},{id: 1, value: false},{id: 2, value: false},{id: 3, value: false},{id: 4, value: false}],
 		[{id: 5, value: false},{id: 6, value: false},{id: 7, value: false},{id: 8, value: false},{id: 9, value :false}],
@@ -9,17 +11,16 @@ var navalGame = (function(){
 		[{id: 20, value: false},{id: 21, value: false},{id: 22, value: false},{id: 23, value: false},{id: 24, value: false}]
 	]
 
-	console.log(game.dashboard);
-
 	game.populateBoats = function(){
 		for(let i = 0 ; i < 5 ; i++){
 			axisX = Math.floor((Math.random() * 4));
 			axisY = Math.floor((Math.random() * 4));
+
 			if (game.isTheCellAvaliable(axisX, axisY)) {
 				game.dashboard[axisX][axisY].value = true;
 				console.log("propiedad", game.dashboard[axisX][axisY]);
 			}else{
-				continue;
+				i--;
 			}
 		}
 
@@ -33,19 +34,20 @@ var navalGame = (function(){
 	game.addEventToButtons = function(){
 		for(let i = 0 ; i < (game.dashboard.length * game.dashboard.length) ; i++){
 			var btn = $('#'+i);
-
 			btn.click(({axisX: btn.attr('axisX'), axisY: btn.attr('axisY')}), game.shootWeapon);
 		}
 	}
 
 	game.shootWeapon = function(event){
-		console.log("id", event.target.id, "x",event.data.axisX, "y", event.data.axisY);
 		x = parseInt(event.data.axisX);
 		y = parseInt(event.data.axisY);
-		if ( event.target.id === game.dashboard[x][y] ) {
-			console.log("jaja, looooooser");
-		}else{
+		
+		if ( game.dashboard[x][y].value === true ) {
 			console.log("dammm!!!, you did it");
+			$('#' + event.target.id).addClass('boat-destroyed');
+		}else{
+			console.log("jaja, looooooser");
+			$('#' + event.target.id).addClass('no-boat');
 		}
 	}
 
